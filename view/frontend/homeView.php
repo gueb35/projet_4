2192da526c84bf3f649e0b5b16af8123a4905993
alban <aletsy@sfr.fr>
2018-07-24 12:53:35 +0200

    Fabrication de la première de couverture sur la vue homeView.php

--- view/frontend/homeView.php
+++ view/frontend/homeView.php
@@ -2,11 +2,16 @@
 <?php ob_start(); ?><!--définit le contenu de la page, ob_start mémorise toute la sortie html-->
 
     <div class="text-center"> 
-        <h1>Billet simple pour l'alaska</h1>
-        <p>Bienvenue sur mon site!</p>
+        <h1>Bienvenue sur mon site!</h1>
     
     <p>Pour ce nouveau roman, j'ai décidé de vous le faire découvrir par épisode directement sur ce site.<br/></p>
     <p>Afin d'être plus proche de vous, vous pourrez commenter chaque épisode alors, n'hésitez pas, donnez vos impressions!!</p>
+    </div>
+
+    <div class="episode text-center">
+        <h2>Billet simple pour l'alaska</h2>
+        <p class="auteur_couverture">Auteur :</p>
+        <p>Jean Forteroche</p>
     </div>
 
 
