7e6ae5ddabb0992b7f6e96e0f5ab31b57152a053
alban <aletsy@sfr.fr>
2018-07-24 10:29:19 +0200

    Rédaction du manager permettant la connexion à la bdd

---
+++ model/Manager.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace alban\project_4\model;
+
+class Manager
+{
+    protected function dbConnect()
+    {
+        $db = new \PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', '');
+        return $db;
+    }
+}
