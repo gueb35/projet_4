578239f1214357cf4207318bd89846a71f9f88e2
alban <aletsy@sfr.fr>
2018-07-26 13:01:01 +0200

    Gestion de l'ensemble des erreurs affichées sur la vue errorView.php

--- view/frontend/errorView.php
+++ view/frontend/errorView.php
@@ -1,6 +1,9 @@
 <?php ob_start(); ?>
 
-    <?=  'La connexion à la base de données à échouée, vérifiez la réquete de connexion à la base de données.Un erreur d\'orthographe est vite arrivée !' ?> 
+<div class="errorMessage">
+    <!-- <?=  'La connexion à la base de données à échouée, vérifiez la réquete de connexion à la base de données.Un erreur d\'orthographe est vite arrivée !' ?>  -->
+    <?=  'Erreur : ' . $e->getMessage(); ?>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 
