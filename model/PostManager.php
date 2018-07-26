5ee7a7623598455b6be2f876713ab47a0e22df42
alban <aletsy@sfr.fr>
2018-07-26 15:11:13 +0200

    Mise en place du namespace et écriture de la réquête pour insérer un épisode en bdd ds postManager.php

--- model/PostManager.php
+++ model/PostManager.php
@@ -1,17 +1,25 @@
 <?php
 
-namespace alban\project4\model;
+namespace alban\project_4\model;
 
 require_once('../model/Manager.php');//fait appel au fichier "Manager" pour la connexion à la bdd
 //cela évite de dupliquer le code de connexion à la bdd
 
 class PostManager extends Manager
 {
-    public function getPosts()//récupère tous les épisodes
+    public function postText($resultat)//fonction qui fait une requète pour insérer des épisodes en bdd
     {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id, author, title, content, FROM author ORDER BY id');
+        $comments = $db->prepare('INSERT INTO author(resultat) VALUES(?, NOW())');
+        $affectedLines = $comments->execute(array($resultat));
 
-        return $req;
+        return $affectedLines;
     }
+    // public function getPosts()//récupère tous les épisodes
+    // {
+    //     $db = $this->dbConnect();
+    //     $req = $db->query('SELECT id, author, title, content, FROM author ORDER BY id');
+
+    //     return $req;
+    // }
 }
