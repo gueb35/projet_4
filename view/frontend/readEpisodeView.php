6edd058db80e1a47da5f6a331b9345b7f9149755
alban <aletsy@sfr.fr>
2018-07-27 09:28:41 +0200

    Création du fichier readEpisodeView.php

---
+++ view/frontend/readEpisodeView.php
@@ -0,0 +1,11 @@
+<?php $title = 'Bienvenue sur la page de lecture des épisodes !'; ?><!--définit le titre de la page, celui-ci sera inséré ds la balise title ds le template-->
+<?php ob_start(); ?><!--définit le contenu de la page, ob_start mémorise toute la sortie html-->
+
+    <div class="episode text-center">
+    </div>
+
+
+<?php $content = ob_get_clean(); ?><!--récupère le contenu généré et met tout ds $content-->
+
+<?php require('template.php'); ?><!--appelle le template pour récupérer les variables $title et $content-->
+
