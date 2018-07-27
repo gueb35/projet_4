9ae37fbebb6072fb9fbf06c434ae8e3b33afd514
alban <aletsy@sfr.fr>
2018-07-27 16:34:12 +0200

    Création de la fonction showComments() pour afficher les commentaires ds le controleur

--- controller/frontend.php
+++ controller/frontend.php
@@ -63,22 +63,18 @@ function addComment($postId, $author, $comment)//fonction qui permet d'envoyer u
     else {
         header('Location: index.php?action=accessEpisode&id=' . $postId);
     }
+
+function showComments($id)//fonction pour afficher les commentaires
+{
+    $commentManager = new CommentManager();// Création d'un objet(instance)
+    $comments = $commentManager->getComments($_GET['id']);// Appel d'une fonction de cet objet(invoquer la méthode de cet objet),
+    // elle fait une requète ds CommentManager.php pour afficher le commentaire associé au billet selectioné
+
+    require('../view/frontend/readEpisodeView.php');//transmet les données(requete stockées ds des variables) à l'affichage (vue)
 }
-// function addComment($postId, $author, $comment)//fonction qui permet d'envoyer un commentaire
-// {
-//     $commentManager = new CommentManager();// Création d'un objet(instance)
-
-//     $affectedLines = $commentManager->postComment($postId, $author, $comment);// Appel d'une fonction de cet objet(invoquer la méthode de cet objet),
-//     //elle fait une requète ds CommentManager pour créer un commentaire
-
-//     if ($affectedLines === false) {
-//         throw new Exception('Impossible d\'ajouter le commentaire !');
-//     }
-//     else {
-//         header('Location: index.php?action=post&id=' . $postId);
-//     }
 
 
+}
 // function listPosts()//fonction utilisé pour lister tous les billets
 // {
 //     $postManager = new PostManager(); // Création d'un objet(instance)
@@ -102,21 +98,6 @@ function addComment($postId, $author, $comment)//fonction qui permet d'envoyer u
 
 //     require('../view/frontend/postView.php');//transmet les données(requete stockées ds des variables) à l'affichage (vue)
 // }
-
-// function postA()//fonction pour afficher le billet selectionné et les commentaires associés
-// {
-//     $postManagerA = new PostManager();// Création d'un objet(instance)
-//     $commentManagerA = new CommentManager();// Création d'un objet(instance)
-
-//     $postA = $postManagerA->getPost($_GET['post_id']);// Appel d'une fonction de cet objet(invoquer la méthode de cet objet),
-//     // elle fait une requète préparé pour afficher le billet selectioné
-//     $realyThisComment = $commentManagerA->getComment($_GET['id']);// Appel d'une fonction de cet objet(invoquer la méthode de cet objet),
-//     // elle fait une requète ds CommentManager.php pour afficher le commentaire associé au billet selectioné
-
-//     require('../view/frontend/changePostView.php');
-// }
-
-
 
 // function modifyComment($id, $comment)//fonction qui permet de modifier un commentaire
 // {
