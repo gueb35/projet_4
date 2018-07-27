0e3565c66b7ebbc47af8d6292752711367936157
alban <aletsy@sfr.fr>
2018-07-27 10:35:02 +0200

    Rédaction du code de la vue readEpisodeView.php et mise en place d'un formulaire pour l'ajout de commentaires

--- view/frontend/readEpisodeView.php
+++ view/frontend/readEpisodeView.php
@@ -1,9 +1,30 @@
 <?php $title = 'Bienvenue sur la page de lecture des épisodes !'; ?><!--définit le titre de la page, celui-ci sera inséré ds la balise title ds le template-->
 <?php ob_start(); ?><!--définit le contenu de la page, ob_start mémorise toute la sortie html-->
 
-    <div class="episode text-center">
+    <div class="text-center"> 
+        <h1>Bonne lecture!</h1>
     </div>
 
+    <div class="row">
+        <div class="episode1andmore col-md-offset-1 col-md-5">
+        </div>
+        <div class="col-md-offset-1 col-md-3">
+            <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+                <p><strong>N'oubliez pas de poster votre commentaire !</strong></p>
+                    <div>
+                        <label for="author">Auteur</label><br />
+                        <input type="text" id="author" name="author" />
+                    </div>
+                    <div>
+                        <label for="comment">Commentaire</label><br />
+                        <textarea id="comment" name="comment"></textarea>
+                    </div>
+                    <div>
+                        <input type="submit" />
+                    </div>
+            </form>
+        </div>
+    </div>
 
 <?php $content = ob_get_clean(); ?><!--récupère le contenu généré et met tout ds $content-->
 
