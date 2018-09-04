8cd0acd1ee57723389453096e2b6225229f87da0
alban <aletsy@sfr.fr>
2018-09-04 10:19:38 +0200

    Mise place de l'éditeur tinymce et suppression de l'ancien éditeur(editeur.js).Fonctionnalité opérationnelle!

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -7,7 +7,14 @@
         <title><?= $title ?></title>
         <link rel="stylesheet" href="../public/css/style.css" />
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
-        <script src="../public/js/editeur.js"></script>
+        <script src="../public/js/tinymce/tinymce.min.js"></script>
+        <script type="text/javascript">
+            tinymce.init({
+                selector: '#resultat',
+                language: 'fr_FR',
+                language_url: '../public/js/tinymce/langs/fr_FR.js'
+            });
+        </script>
     </head>
         
     <body>
