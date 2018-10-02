ae7c0582c424be2e267fae7f083a95a7e602d535
alban <aletsy@sfr.fr>
2018-10-02 09:20:00 +0200

    Modification des noms de table en bdd et des fichiers utilisant ses tables.

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -10,7 +10,7 @@ class CommentManager extends Manager
     public function postComment($postId, $author, $comment)//fonction qui fait une requète pour créer des commentaires
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('INSERT INTO comment_space (post_id, author, comment, creation_date) VALUES(?, ?, ?, NOW())');
+        $comments = $db->prepare('INSERT INTO user (post_id, author, comment, creation_date) VALUES(?, ?, ?, NOW())');
         $affectedLines = $comments->execute(array($postId, $author, $comment));
 
         return $affectedLines;
@@ -18,7 +18,7 @@ class CommentManager extends Manager
     public function getComments($postId)//fonction qui fait une requète pour récupèrer les commentaires
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, post_id, author, moderated, comment, creation_date, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM comment_space WHERE post_id = ? ORDER BY creation_date DESC');
+        $comments = $db->prepare('SELECT id, post_id, author, moderated, comment, creation_date, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM user WHERE post_id = ? ORDER BY creation_date DESC');
         $comments->execute(array($postId));
 
         return $comments;
@@ -26,13 +26,13 @@ class CommentManager extends Manager
     public function accessModerateCommentView()//requète avec jointure entre les 2 tables
     {
         $db = $this->dbConnect();
-        $commentsModerate = $db->prepare('SELECT co.comment AS commentModerate, co.author 
-        AS authorComMod, co.id AS idComMod, co.moderated AS comMod, au.title AS titleComMod,
+        $commentsModerate = $db->prepare('SELECT us.comment AS commentModerate, us.author 
+        AS authorComMod, us.id AS idComMod, us.moderated AS comMod, po.title AS titleComMod,
         DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr 
-        FROM comment_space AS co
-        INNER JOIN author AS au
-        ON co.post_id = au.id 
-        WHERE co.moderated = "signalé" || co.moderated = "modéré" ORDER BY creation_date DESC');
+        FROM user AS us
+        INNER JOIN posts AS po
+        ON us.post_id = po.id 
+        WHERE us.moderated = "signalé" || us.moderated = "modéré" ORDER BY creation_date DESC');
         $commentsModerate->execute(array());
 
         return $commentsModerate;
@@ -40,7 +40,7 @@ class CommentManager extends Manager
     public function pushModerated($id)//fonction qui fait une requete pour modérer un commentaire
     {
         $db = $this->dbConnect();
-        $moderate = $db->prepare('UPDATE comment_space SET moderated = "modéré" WHERE id = :newId');
+        $moderate = $db->prepare('UPDATE user SET moderated = "modéré" WHERE id = :newId');
         $moderate->execute(array(
             'newId' => $id
             ));
@@ -48,14 +48,14 @@ class CommentManager extends Manager
     public function moderated($id)//fonction qui fait une requète pour signaler un commentaire
     {
         $db = $this->dbConnect();
-        $moderate = $db->prepare('UPDATE comment_space SET moderated = "signalé" WHERE id = :newId');
+        $moderate = $db->prepare('UPDATE user SET moderated = "signalé" WHERE id = :newId');
         $moderate->execute(array(
             'newId' => $id
             ));
     }
     public function deleteComment($postId){//efface les commentaires associés à un épisode
         $db = $this->dbConnect();
-        $delete = $db->prepare('DELETE FROM comment_space WHERE post_id = ?');
+        $delete = $db->prepare('DELETE FROM user WHERE post_id = ?');
         $delete->execute(array($postId));
     }
 }
--- model/PostManager.php
+++ model/PostManager.php
@@ -9,7 +9,7 @@ class PostManager extends Manager
     public function postText($content_post, $title)//fonction qui fait une requète pour insérer des épisodes en bdd
     {
         $db = $this->dbConnect();
-        $post = $db->prepare('INSERT INTO author(content_post, title) VALUES(?, ?)');
+        $post = $db->prepare('INSERT INTO posts(content_post, title) VALUES(?, ?)');
         $affectedLines = $post->execute(array($content_post, $title));
 
         return $affectedLines;
@@ -17,7 +17,7 @@ class PostManager extends Manager
     public function getPosts()//récupère tous les épidodes
     {
         $db = $this->dbConnect();
-        $posts = $db->prepare('SELECT id, title, content_post, SUBSTRING(content_post, 1, 500) AS short_post FROM author');
+        $posts = $db->prepare('SELECT id, title, content_post, SUBSTRING(content_post, 1, 500) AS short_post FROM posts');
         $posts->execute(array());
     
         return $posts;    
@@ -25,7 +25,7 @@ class PostManager extends Manager
     public function getPost($id)//récupère un épisode
     {
         $db = $this->dbConnect($id);
-        $req = $db->prepare('SELECT id, title, content_post FROM author WHERE id = ?');
+        $req = $db->prepare('SELECT id, title, content_post FROM posts WHERE id = ?');
         $req->execute(array($id));
         $post = $req->fetch();
     
@@ -34,7 +34,7 @@ class PostManager extends Manager
     public function getPostInferior($id)//récupère l'épisode précédent
     {
         $db = $this->dbConnect($id);
-        $req = $db->prepare('SELECT id FROM author WHERE id < ? ORDER BY id DESC LIMIT 1');
+        $req = $db->prepare('SELECT id FROM posts WHERE id < ? ORDER BY id DESC LIMIT 1');
         $req->execute(array($id));
         $postId = $req->fetch();
     
@@ -43,7 +43,7 @@ class PostManager extends Manager
     public function getPostSuperior($id)//récupère l'épisode suivant
     {
         $db = $this->dbConnect($id);
-        $req = $db->prepare('SELECT id FROM author WHERE id > ? LIMIT 1');
+        $req = $db->prepare('SELECT id FROM posts WHERE id > ? LIMIT 1');
         $req->execute(array($id));
         $postId = $req->fetch();
         
@@ -52,7 +52,7 @@ class PostManager extends Manager
     public function updatePost ($id, $content_post, $updateTitle)
     {
         $db = $this->dbConnect($id);
-        $update = $db->prepare('UPDATE author SET content_post = :newcontent_post, title = :newTitle WHERE id = :newId');
+        $update = $db->prepare('UPDATE posts SET content_post = :newcontent_post, title = :newTitle WHERE id = :newId');
         $update->execute(array(
             'newcontent_post' => $content_post,
             'newTitle' => $updateTitle,
@@ -61,7 +61,7 @@ class PostManager extends Manager
     }     
     public function deletePost($id){
         $db = $this->dbConnect();
-        $delete = $db->prepare('DELETE FROM author WHERE id = ? LIMIT 1');
+        $delete = $db->prepare('DELETE FROM posts WHERE id = ? LIMIT 1');
         $delete->execute(array($id));
     }
     
