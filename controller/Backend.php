10240d82e099906a1ad6d32655290f5d3c284c0b
alban <aletsy@sfr.fr>
2018-10-04 15:52:21 +0200

    Création d'un constructeur ds les controleurs pour éviter la duplication de code.Fonctionnel!

--- controller/Backend.php
+++ controller/Backend.php
@@ -10,8 +10,16 @@ use \alban\project_4\model\CommentManager;
 // require_once('model/PostManager.php');//permet d'avoir accès aux méthodes du modèle
 require_once('model/CommentManager.php');//permet d'avoir accès aux méthodes du modèle
 
-class Backend extends CommentManager//permet d'hériter des méthodes de la class CommentManager donc d'utiliser $this
+class Backend//permet d'hériter des méthodes de la class CommentManager donc d'utiliser $this
 {
+    private $_postManager;
+    private $_commentManager;
+    
+    public function __construct()
+    {
+        $this->_postManager = new PostManager();
+        $this->_commentManager = new CommentManager();
+    }
     /************fonctions pour accéder aux vues directement*************/
     public function accessAdministrator()
     {
@@ -23,56 +31,51 @@ class Backend extends CommentManager//permet d'hériter des méthodes de la clas
     }
     /*************************/
     public function sendText(string $content_post, string $title){//permet d'envoyer un épisode en bdd
-        $postManager = new PostManager();
-        $postManager->postText($content_post, $title);
-        $posts = $postManager->getPosts();
+        $this->_postManager->postText($content_post, $title);
+        $posts = $this->_postManager->getPosts();
 
         require('view/backend/administratorHomeView.php');
     }
     /***************************/
     public function postsAdministrator()//fonction pour afficher tous les épisodes sur la page d'accueil de l'espace administrateur
     {
-        $postManager = new PostManager();
-        $posts = $postManager->getPosts();
+        $posts = $this->_postManager->getPosts();
 
         require('view/backend/administratorHomeView.php');
     }
     /***************************/
     public function moderatedComment(string $commentId, int $id)//fonction pour signaler un commentaire
     {
-        $this->moderated($commentId);
+        $this->_commentManager->moderated($commentId);
     }
     /***************************/
     public function pushModerateComment(int $id)//permet la modération d'un commentaire
     {
-        $this->pushModerated($id);
-        $commentManager = new CommentManager();
-        $commentsModerate = $commentManager->accessModerateCommentView();
+        $this->_commentManager->pushModerated($id);
+        $commentsModerate = $this->_commentManager->accessModerateCommentView();
         
         require('view/backend/administratorModerateComment.php');
     }
     /***************************/
     public function accessModerateCommentView()//récupère des données ds 2 tables différentes
     {
-        $commentManager = new CommentManager();
-        $commentsModerate = $commentManager->accessModerateCommentView();
+        // $commentManager = new CommentManager();
+        $commentsModerate = $this->_commentManager->accessModerateCommentView();
 
         require('view/backend/administratorModerateComment.php');
     }
     /***************************/
     public function accessWysiwyg(int $id)
     {
-        $postManager = new PostManager();
-        $post = $postManager->getPost($id);
+        $post = $this->_postManager->getPost($id);
 
         require('view/backend/administratorUpdateView.php');
     }
     /***************************/
     public function updatePost (int $id, string $content_post, string $updateTitle)
     {
-        $postManager = new PostManager();
-        $postManager->updatePost($id, $content_post, $updateTitle);
-        $posts = $postManager->getPosts();
+        $this->_postManager->updatePost($id, $content_post, $updateTitle);
+        $posts = $this->_postManager->getPosts();
 
         $this->deleteComment($id);
         
@@ -81,9 +84,8 @@ class Backend extends CommentManager//permet d'hériter des méthodes de la clas
     /***************************/
     public function deletePost(int $id)
     {
-        $postManager = new PostManager();
-        $postManager->deletePost($id);
-        $posts = $postManager->getPosts();
+        $this->_postManager->deletePost($id);
+        $posts = $this->_postManager->getPosts();
 
         $this->deleteComment($id);
 
--- controller/Frontend.php
+++ controller/Frontend.php
@@ -10,8 +10,16 @@ use \alban\project_4\model\CommentManager;
 require_once('model/PostManager.php');//permet d'avoir accès aux méthodes du modèle
 // require_once('model/CommentManager.php');//permet d'avoir accès aux méthodes du modèle
 
-class Frontend extends PostManager
+class Frontend
 {
+    private $_postManager;
+    private $_commentManager;
+    
+    public function __construct()
+    {
+        $this->_postManager = new PostManager();
+        $this->_commentManager = new CommentManager();
+    }
     /************fonctions pour accéder aux vues*************/
     public function showHomeView()//appelé si aucun paramètre action n'est présent
     {
@@ -20,30 +28,28 @@ class Frontend extends PostManager
     /***************************/
     public function posts()//fonction pour afficher tous les épisodes sur la page d'accueil de l'espace lecture
     {
-        $posts = $this->getPosts();
+        $posts = $this->_postManager->getPosts();
 
         require('view/frontend/readPostsHomeView.php');
     }
     /***************************/
     public function post(int $id)//fonction pour afficher l'épisode et ses commentaires
     {
-        $post = $this->getPost($id);
+        $post = $this->_postManager->getPost($id);
         
-        $previousPostId = $this->getPostInferior($id);
-        $nextPostId = $this->getPostSuperior($id);
+        $previousPostId = $this->_postManager->getPostInferior($id);
+        $nextPostId = $this->_postManager->getPostSuperior($id);
 
-        $commentManager = new CommentManager();
-        $comments = $commentManager->getComments($id);
+        $comments = $this->_commentManager->getComments($id);
 
         require('view/frontend/readPostView.php');
     }
     /********************************/
     public function addComment(int $postId, string $author, string $comment)//fonction pour envoyer un commentaire
     {
-        $post = $this->getPost($postId);
+        $post = $this->_postManager->getPost($postId);
 
-        $commentManager = new CommentManager();
-        $affectedLines = $commentManager->postComment($postId, $author, $comment);
+        $affectedLines = $this->_commentManager->postComment($postId, $author, $comment);
         if($affectedLines === false){
             throw new Exception('Impossible d\'ajouter le commentaire !');
         }
