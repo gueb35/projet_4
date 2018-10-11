d121345e3931c70e9b866d5d893d8987a7bea856
alban <aletsy@sfr.fr>
2018-10-11 09:38:00 +0200

    Commentaires dockbloc ds Manager.php.

--- model/Manager.php
+++ model/Manager.php
@@ -4,6 +4,14 @@ namespace alban\projet_4\model;
 
 abstract class Manager
 {
+    /**
+     * fonction pour la connexion à la bdd
+     * 
+     * @param host nom d'hôte
+     * @param dbname nom de la bdd
+     * @param string nom d'utilisateur
+     * @param string mdp bdd
+     */
     protected function dbConnect()
     {
         $db = new \PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', '');
